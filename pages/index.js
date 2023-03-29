45e8cd3d5d4271dce21c1dcfb7413926742ba6e7
Partha Chakraborty <partha.c1999@gmail.com>
2023-03-29 22:27:21 +0530

    Layout added to all the pages with navbar and routes pages added from components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
 
-const HomePage=()=>{
-    return <h1>The Home page</h1>
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A first meetup',
+        image: 'https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVldHVwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        address: 'Sector 5,Kolkata,West Bengal',
+        description: 'First meetup hosted by us'
+    },
+    {
+        id: 'm2',
+        title: 'React meetup',
+        image: 'https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVldHVwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        address: 'Sector 6,Kolkata,West Bengal',
+        description: 'meetup details will be forwarded to you'
+    }
+]
+const HomePage = () => {
+    return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    )
 }
-
 export default HomePage;
